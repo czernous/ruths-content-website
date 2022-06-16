794bfbc1f6fd11daf0b3338ac0df5afe4c9d2edd
Anton Chernous <a.chernous@notamedia.ru>
2022-06-17 00:34:44 +0300

    update docker compose, add scripts

---
+++ scripts/docker-run-dev.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker rm -f app-admin
+docker-compose -f docker-compose.yml -f docker-compose.development.yml up --build -d 
+echo -e "\033[33mremoving dangling images"
+docker image prune -f
